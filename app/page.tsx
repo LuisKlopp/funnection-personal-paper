33185cd150b9b003e74c471c607e628cacb4a991
LuisKlopp <fbgus333@gmail.com>
2024-07-19 15:56:02 +0900

    Feat: main 유저리스트 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,8 @@
-import { fetchUserList } from "@/api/fetchUserList";
-import Link from "next/link";
-
 export default async function Home() {
-  const userData = await fetchUserList();
-
   return (
     <main className="flex flex-col items-center justify-between">
       <div className="flex h-[100dvh] w-full max-w-xl flex-col items-center border-black bg-white text-mainColor">
         <span className="p-4 text-4xl">Funnection</span>
-        <div className="flex flex-col gap-4">
-          {userData.map((user) => (
-            <Link key={user.id} href={`/${user.generateString}`}>
-              <div className="w-[100px] border-2 border-black p-4 text-2xl">
-                <span>{user.nickname}</span>
-              </div>
-            </Link>
-          ))}
-        </div>
       </div>
     </main>
   );
