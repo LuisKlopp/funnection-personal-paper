c49aee8640432e202c6bc8ad2469877564c44ae2
LuisKlopp <fbgus333@gmail.com>
2024-07-24 16:39:18 +0900

    Feat: main userlist 지우기

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-import { fetchUserList } from "@/api/fetchUserList";
-import Link from "next/link";
+// import { fetchUserList } from "@/api/fetchUserList";
+// import Link from "next/link";
 
 export default async function Home() {
-  const userList = await fetchUserList();
+  // const userList = await fetchUserList();
 
   return (
     <main className="flex flex-col items-center justify-between">
       <div className="flex h-[100dvh] w-full max-w-xl flex-col items-center border-black bg-white text-mainColor">
         <span className="p-4 text-4xl">Funnection</span>
         <div className="flex flex-wrap gap-4 text-2xl">
-          {userList.map((user) => (
+          {/* {userList.map((user) => (
             <div key={user.id} className="border-2 border-black p-4">
               <Link href={user.generateString}>{user.nickname}</Link>
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
     </main>
